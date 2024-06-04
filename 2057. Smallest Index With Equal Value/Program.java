c4c485f354168094a138eebf20f62c0c75214961
Tej Dekiwadiya <105840549+tejdekiwadiya@users.noreply.github.com>
2024-06-04 17:38:17 +0530

    update

--- 2057. Smallest Index With Equal Value/Program.java
+++ 2057. Smallest Index With Equal Value/Program.java
@@ -1,3 +1,5 @@
+// // https://leetcode.com/problems/smallest-index-with-equal-value/description/
+
 public class Program {
     public static void main(String[] args) {
         int[] nums = { 0, 1, 2 };
