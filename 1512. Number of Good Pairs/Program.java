e0896b7f21d67b4ad7c7cade28f1cbdd8faf3afa
Tej Dekiwadiya <105840549+tejdekiwadiya@users.noreply.github.com>
2024-06-05 17:15:18 +0530

    Add files via upload

---
+++ 1512. Number of Good Pairs/Program.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/number-of-good-pairs/description/
+
+public class Program {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 1, 1, 3 };
+        int ans = numIdenticalPairs(nums);
+        System.out.println(ans);
+    }
+
+    public static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j] && i < j) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
