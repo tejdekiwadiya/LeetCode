719079cc28add2dc8dfa7fcf75030805217bc7b0
Tej Dekiwadiya <105840549+tejdekiwadiya@users.noreply.github.com>
2024-06-18 22:12:00 +0530

    Add files via upload

---
+++ 0160. Intersection of Two Linked Lists/Program.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
+public class Program {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            if (a == null) {
+                a = headB;
+            } else {
+                a = a.next;
+            }
+            if (b == null) {
+                b = headA;
+            } else {
+                b = b.next;
+            }
+        }
+        return a;
+    }
+}
