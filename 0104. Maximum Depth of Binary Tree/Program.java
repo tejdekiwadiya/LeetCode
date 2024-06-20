7642e312324d560b616233284b1e4252bf5f6b52
Tej Dekiwadiya <105840549+tejdekiwadiya@users.noreply.github.com>
2024-06-21 03:12:25 +0530

    Add files via upload

---
+++ 0104. Maximum Depth of Binary Tree/Program.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+public class Program {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return left > right ? left + 1 : right + 1;
+    }
+}
