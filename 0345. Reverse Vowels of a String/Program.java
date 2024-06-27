d7aa3467f296751d2600a0841d9696e68c983e94
Tej Dekiwadiya <105840549+tejdekiwadiya@users.noreply.github.com>
2024-06-27 17:16:52 +0530

    update

--- 0345. Reverse Vowels of a String/Program.java
+++ 0345. Reverse Vowels of a String/Program.java
@@ -10,33 +10,34 @@ public class Program {
     public static String reverseVowels(String s) {
         char[] char_temp = s.toCharArray();
         int right = char_temp.length - 1;
-        int i = 0;
-        while (i < right) {
-            if (char_temp[i] == 'a' || char_temp[i] == 'e' || char_temp[i] == 'i' || char_temp[i] == 'o'
-                    || char_temp[i] == 'u' || char_temp[i] == 'A' || char_temp[i] == 'E' || char_temp[i] == 'I'
-                    || char_temp[i] == 'O'
-                    || char_temp[i] == 'U') {
+        int left = 0;
+        while (left < right) {
+            if (char_temp[left] == 'a' || char_temp[left] == 'e' || char_temp[left] == 'i' || char_temp[left] == 'o'
+                    || char_temp[left] == 'u' || char_temp[left] == 'A' || char_temp[left] == 'E'
+                    || char_temp[left] == 'I'
+                    || char_temp[left] == 'O'
+                    || char_temp[left] == 'U') {
                 if (char_temp[right] == 'a' || char_temp[right] == 'e'
                         || char_temp[right] == 'i' || char_temp[right] == 'o'
                         || char_temp[right] == 'u' || char_temp[right] == 'A' || char_temp[right] == 'E'
                         || char_temp[right] == 'I' || char_temp[right] == 'O'
                         || char_temp[right] == 'U') {
-                    char temp = char_temp[i];
-                    char_temp[i] = char_temp[right];
+                    char temp = char_temp[left];
+                    char_temp[left] = char_temp[right];
                     char_temp[right] = temp;
                     right--;
-                    i++;
+                    left++;
                 } else {
                     right--;
                 }
             } else {
-                i++;
+                left++;
             }
         }
 
         StringBuilder ans = new StringBuilder();
-        for (int k = 0; k < char_temp.length; k++) {
-            ans.append(char_temp[k]);
+        for (int i = 0; i < char_temp.length; i++) {
+            ans.append(char_temp[i]);
         }
         return ans.toString();
     }
