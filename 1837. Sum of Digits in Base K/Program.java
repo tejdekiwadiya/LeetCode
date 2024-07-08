30cb003299ff4a9111ef29fa7ce7c48b2f319306
Tej Dekiwadiya <workfor.td@gmail.com>
2024-07-08 19:31:25 +0530

    commit

---
+++ 1837. Sum of Digits in Base K/Program.java
@@ -0,0 +1,27 @@
+public class Program {
+    public static void main(String args[]) {
+        int n = 34;
+        int k = 6;
+        int ans = sumBase(n, k);
+        System.out.println(ans);
+    }
+
+    public static int sumBase(int n, int k) {
+        String digit = convertToBase(n, k);
+        int num = 0;
+        for (int i = digit.length() - 1; i >= 0; i--) {
+            String temp = String.valueOf(digit.charAt(i));
+            num += Integer.parseInt(temp);
+        }
+        return num;
+    }
+
+    private static String convertToBase(int n, int k) {
+        String ans = "";
+        while (n > 0) {
+            ans += n % k;
+            n = n / k;
+        }
+        return ans;
+    }
+}
