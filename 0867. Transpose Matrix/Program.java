e528a3f7bf0d94b29f9f833a0c12d8a630bb122d
Tej Dekiwadiya <workfor.td@gmail.com>
2024-07-18 00:03:19 +0530

    commit

---
+++ 0867. Transpose Matrix/Program.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/transpose-matrix/description/
+
+import java.util.Arrays;
+
+public class Program {
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int[][] ans = transpose(matrix);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    public static int[][] transpose(int[][] matrix) {
+        int[][] ans = new int[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        return ans;
+    }
+}
